cdfc57d860e09f49e46d493256da635b70f8c4d6
Alan <67981085+4l4n-75@users.noreply.github.com>
2021-10-17 16:37:47 -0500

    Update ControladorUsuario.java

--- src/main/java/com/unitec/amigos/ControladorUsuario.java
+++ src/main/java/com/unitec/amigos/ControladorUsuario.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//// comentario de prueba
 @RestController
 @RequestMapping("/api")
 public class ControladorUsuario {
